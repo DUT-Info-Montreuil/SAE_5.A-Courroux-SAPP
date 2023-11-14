7d012af0276752fd73a2762ff387f211b83bf9f4
Nicolas RAMIREZ <nicolas.ramirez2305@hotmail.com>
2023-11-14 16:09:23 +0100

    ajout de la sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  
 })
 export class SidebarComponent {
+  isActive: boolean = false;
+  isActiveMessagerie: boolean = false;
+  isActiveVisioConferences: boolean = false;
 
+  toggleSidebar(){
+    this.isActive = !this.isActive
+  }
+
+  toggleMessagerie() {
+    this.isActiveMessagerie = !this.isActiveMessagerie;
+    this.isActiveVisioConferences = false;
+  }
+
+  toggleVisioConferences() {
+    this.isActiveVisioConferences = !this.isActiveVisioConferences;
+    this.isActiveMessagerie = false;
+  }
 }
 
