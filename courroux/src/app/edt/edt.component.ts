47d83be65a86497637707b35b081db88921e8ce1
ismaelargence <ismaelargence3@gmail.com>
2023-11-20 14:53:16 +0100

    pour merge

--- courroux/src/app/edt/edt.component.ts
+++ courroux/src/app/edt/edt.component.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit } from '@angular/core';
 import { CalendarEvent, CalendarView, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/moment';
 import * as moment from 'moment';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DatePipe } from '@angular/common';
-import { EventColor } from 'calendar-utils';
 import { EdtService } from '../services/edt.service';
 
 export function momentAdapterFactory() {
@@ -18,7 +17,7 @@ export function momentAdapterFactory() {
   styleUrls: ['./edt.component.scss']
 })
 
-export class EdtComponent {
+export class EdtComponent{
 
   formAddEvent = new FormGroup({
     cours: new FormControl(""),
@@ -55,8 +54,15 @@ export class EdtComponent {
     return this.events.indexOf(event.event);
   }
 
-  constructor(private datePipe: DatePipe, private edtService: EdtService) {
-    this.loadEvents();
+  constructor(
+    private datePipe: DatePipe,
+    private edtService: EdtService,
+    private cdr: ChangeDetectorRef,
+    private zone: NgZone) {
+      this.loadEvents();
+      this.profs = this.edtService.getProfs();
+      this.ressources = this.edtService.getRessources();
+      this.salles = this.edtService.getSalles();
   }
 
   openModalMod() {
@@ -70,10 +76,6 @@ export class EdtComponent {
   }
 
   openModalAdd() {
-
-    this.profs = this.edtService.getProfs();
-    this.ressources = this.edtService.getNoms();
-    this.salles = this.edtService.getSalles();
     this.showModalAdd = true;
   }
   
--- courroux/src/app/forms/forms.component.ts
+++ courroux/src/app/forms/forms.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EdtService } from '../services/edt.service';
 
 @Component({
   selector: 'app-forms',
@@ -7,7 +9,43 @@ import { Component } from '@angular/core';
 })
 export class FormsComponent {
 
+  formAddRessource = new FormGroup({
+    nom: new FormControl("", Validators.required),
+    couleur: new FormControl("", Validators.required)
+  })
+
+  formAddSalle = new FormGroup({
+    nom: new FormControl("", Validators.required),
+    nbOrdi: new FormControl("", Validators.required),
+    nbVideoProj: new FormControl("", Validators.required),
+    nbTabNum: new FormControl("", Validators.required)
+  })
+
   public formSelectionne: any = null;
 
-  constructor(){}
+  constructor(private edtService: EdtService){}
+
+  onSubmitAddRessource(){
+    let couleur = this.formAddRessource.value.couleur;
+    let nom = this.formAddRessource.value.nom;
+    if (typeof nom == 'string' && typeof couleur == 'string'){
+      this.edtService.addRessource(nom, couleur);
+      console.log(this.edtService.getRessources());
+    } else {
+      console.error("nom est couleur ne peuvent pas etre null");
+    }
+  }
+
+  onSubmitAddSalle(){
+    let nom = this.formAddSalle.value.nom;
+    let nbOrdi = this.formAddSalle.value.nbOrdi;
+    let nbVideoProj = this.formAddSalle.value.nbVideoProj;
+    let nbTabNum = this.formAddSalle.value.nbTabNum;
+    if (typeof nom === 'string' && typeof nbOrdi === 'number'
+      && typeof nbVideoProj === 'number' && typeof nbTabNum === 'number'){
+      this.edtService.addSalle(nom, nbOrdi, nbVideoProj, nbTabNum);
+    } else {
+      console.error("null");
+    }
+  }
 }
