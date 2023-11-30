e29d536c697efc915361b7c918e31d79c3febb6f
ismael argence <ismaelargence3@gmail.com>
2023-11-30 17:26:44 +0100

    connexion ok w/ jwt

--- src/app/acceuil/acceuil.component.ts
+++ src/app/acceuil/acceuil.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { SessionLoginService } from '../services/session-login.service';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-acceuil',
@@ -21,14 +22,21 @@ export class AcceuilComponent {
   })
 
   constructor(private sessionLoginService: SessionLoginService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private router: Router) { }
 
   onSubmit() {
     if (this.formLogin.valid){
       let identifier = this.formLogin.value.identifier!;
       let password = this.formLogin.value.password!;
       this.sessionLoginService.login(identifier, password).subscribe(
-        (response) => {},
+        (response) => {
+          this.router.navigate(['/edt']);
+          // pour envoyer le groupe quand on load l'edt
+          // this.router.navigate(['/edt'], { queryParams: { key: 'value' } });
+          this.toastr.success('vous etes bien connecté')
+          console.log(response);
+        },
         (error) => {
           this.toastr.error('identifiant et/ou un mot de passe incorect(s)');
         }
--- src/app/services/session-login.service.ts
+++ src/app/services/session-login.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionLoginService {
 
-  LOGIN_URL = 'http://localhost:5000/login';
+  LOGIN_URL = 'http://localhost:8000/login';
 
   constructor(private http: HttpClient) { }
 
   login(identifier: string, password: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
     let credentials = {identifier: identifier, password: password } 
-    return this.http.post(this.LOGIN_URL, credentials);
+    return this.http.post(this.LOGIN_URL, credentials, { headers: headers });
   }
 }
