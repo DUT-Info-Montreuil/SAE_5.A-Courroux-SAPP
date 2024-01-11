108894434799e3fea0d524b0242ac5a3a7d512d6
ismaelargence <ismaelargence3@gmail.com>
2024-01-11 15:40:10 +0100

    la supression d'élève fonctionne

--- src/app/modals/delete-modal/delete-modal.component.ts
+++ src/app/modals/delete-modal/delete-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { concatMap, tap, catchError } from 'rxjs';
 import { GroupService } from 'src/app/_service/group.service';
 import { ResourceService } from 'src/app/_service/resource.service';
 import { RoomService } from 'src/app/_service/room.service';
@@ -109,32 +110,16 @@ export class DeleteModalComponent implements OnInit{
     this.elementASupp = "";
   }
 
-  supprimerEleve(){
-    try {
-      this.supprimerEleveGroupe();
-      this.supprimerEleveUser();
-      this.toastr.success("l'élève(e) a bien été supprimé(e)!");
-    } catch (error) {
-      this.toastr.error("Une erreur est survenue lors de la suppression de l'élève(e)");
-    }
-  }
-
-  supprimerEleveUser(){
+  supprimerEleve() {
     this.studentService.deleteStudent(this.data.element.id).subscribe({
       next: response => {
+        this.toastr.success("l'eleve a bien été supprimé!");
         this.studentsChanged();
       },
-      error: error=> {this.toastr.error("erreur");}
-    });
-    this.elementASupp = "";
-  }
-
-  supprimerEleveGroupe(){
-    this.userGroupService.deleteUserFromGroup(this.data.element.id).subscribe({
-      next: response => {
-        this.groupesChanged();
-      },
-      error: error=> {this.toastr.error("erreur");}
+      error: error=> {
+        this.toastr.error("une erreur est survenue lors de la suppression");
+        console.log(error);
+      }
     });
     this.elementASupp = "";
   }
