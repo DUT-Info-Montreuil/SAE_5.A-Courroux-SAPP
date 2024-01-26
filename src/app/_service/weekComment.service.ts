e428fef746cc64812ae490a28e1c3d97dbe69749
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-01-26 11:45:40 +0100

    add comment weekCommentService

--- src/app/_service/weekComment.service.ts
+++ src/app/_service/weekComment.service.ts
@@ -12,6 +12,12 @@ export class WeekCommentService {
 
     constructor(private http: HttpClient, private utilsService: UtilsService) { }
 
+
+    /*
+        @function getComments
+        @return Observable<WeekComment[]>
+        @desc: get all comments
+    */
     getComments(): Observable<WeekComment[]> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/week/comments`;
         return this.http.get<WeekComment[]>(url, this.utilsService.getJsonHeader())
@@ -19,6 +25,13 @@ export class WeekCommentService {
             retry(1)
         );
     }
+
+    /*
+        @function addComment
+        @param comment: WeekComment
+        @return Observable<WeekComment>
+        @desc: add a comment
+    */
     addComment(comment: WeekComment): Observable<WeekComment> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/week/comment`;
         return this.http.post<WeekComment>(url, comment, this.utilsService.getJsonHeader())
@@ -26,6 +39,13 @@ export class WeekCommentService {
             retry(1)
         );
     }
+
+    /*
+        @function updateComment
+        @param comment: WeekComment
+        @return Observable<WeekComment>
+        @desc: update a comment
+    */
     updateComment(comment: WeekComment): Observable<WeekComment> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/week/comment/${comment.id}`;
         return this.http.put<WeekComment>(url, comment, this.utilsService.getJsonHeader())
@@ -33,6 +53,13 @@ export class WeekCommentService {
             retry(1)
         );
     }
+
+    /*
+        @function getComment
+        @param id: number
+        @return Observable<WeekComment>
+        @desc: get a comment
+    */
     getComment(id: number): Observable<WeekComment> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/week/comment/${id}`;
         return this.http.get<WeekComment>(url, this.utilsService.getJsonHeader())
@@ -40,6 +67,13 @@ export class WeekCommentService {
             retry(1)
         );
     }
+
+    /*
+        @function deleteComment
+        @param id: string
+        @return Observable<WeekComment>
+        @desc: delete a comment
+    */
     deleteComment(id: string): Observable<WeekComment> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/week/comment/${id}`;
         return this.http.delete<WeekComment>(url, this.utilsService.getJsonHeader())
