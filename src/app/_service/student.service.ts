4465d3b4726e69a2918151572ac1a037b813add7
cgrosjean <101260907+CyrilGrj@users.noreply.github.com>
2024-01-26 11:40:46 +0100

    add comment studentService

--- src/app/_service/student.service.ts
+++ src/app/_service/student.service.ts
@@ -14,10 +14,23 @@ export class StudentService {
     private studentRefreshSource = new Subject<void>();
     studentRefresh$ = this.studentRefreshSource.asObservable();
 
+
+    /*
+        @function notifyStudentRefresh
+        @return void
+        @desc: notify all subscribers that a student has been updated
+    */
     notifyStudentRefresh() {
         this.studentRefreshSource.next();
     }
 
+
+
+    /*
+        @function getStudents
+        @return Observable<Student[]>
+        @desc: get all students
+    */
     getStudents(): Observable<Student[]> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/students`;
         return this.http.get<Student[]>(url, this.utilsService.getJsonHeader())
@@ -25,6 +38,14 @@ export class StudentService {
             retry(1)
         );
     }
+
+
+    /*
+        @function addStudent
+        @param student: Student
+        @return Observable<Student>
+        @desc: add a student
+    */
     addStudent(student: Student): Observable<Student> {
         const studentData = this.parseStudent(student);
         let url = `${this.utilsService.getEndPoint().apiUrl}/student`;
@@ -33,6 +54,13 @@ export class StudentService {
             retry(1)
         );
       }
+
+    /*
+        @function updateStudent
+        @param student: Student
+        @return Observable<Student>
+        @desc: update a student
+    */
     updateStudent(student: Student): Observable<Student> {
         const studentData = this.parseStudent(student);
         let url = `${this.utilsService.getEndPoint().apiUrl}/student/${student.id}`;
@@ -41,6 +69,13 @@ export class StudentService {
             retry(1)
         );
       }
+
+    /*
+        @function getStudent
+        @param id: number
+        @return Observable<Student>
+        @desc: get a student
+    */
     getStudent(id: number): Observable<Student> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/student/${id}`;
         return this.http.get<Student>(url, this.utilsService.getJsonHeader())
@@ -48,6 +83,13 @@ export class StudentService {
             retry(1)
         );
     }
+
+    /*
+        @function deleteStudent
+        @param id: number
+        @return Observable<Student>
+        @desc: delete a student
+    */
     deleteStudent(id: number): Observable<Student> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/student/${id}`;
         return this.http.delete<Student>(url, this.utilsService.getJsonHeader())
@@ -55,6 +97,13 @@ export class StudentService {
             retry(1)
         );
     }
+
+    /*
+        @function parseStudent
+        @param student: Student
+        @return any
+        @desc: parse a student to json
+    */
     parseStudent(student: Student): any {
         return {
             "INE": student.INE,
@@ -65,6 +114,13 @@ export class StudentService {
         }
     }
 
+
+    /*
+        @function getStudentsPerGroup
+        @param idGroup: number
+        @return Observable<Student[]>
+        @desc: get all students for a group
+    */
     getStudentsPerGroup(idGroup: number): Observable<Student[]> {
         let url = `${this.utilsService.getEndPoint().apiUrl}/students/groupe/${idGroup}`;
         return this.http.get<Student[]>(url, this.utilsService.getJsonHeader())
